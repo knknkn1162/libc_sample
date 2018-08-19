4d959414fda2b4070f823c2ff870015be365df6f
knknkn <knknkn1162@gmail.com>
2018-08-19 16:17:06 +0900

    implement getpass_r scaffold

---
+++ getpass_r.c
@@ -0,0 +1,67 @@
+#include <signal.h>
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUF_SIZE 100
+
+char *getpass_r(const char *prompt, char *buf, size_t len) {
+  struct termios term;
+  FILE *fp, *outfp;
+  int echo = 0;
+
+  char *ttyName = ctermid(NULL);
+  if((outfp = fp = fopen(ttyName, "w+")) == NULL) {
+    outfp = stderr;
+    fp = stdin;
+  }
+
+  printf("in: %d, out: %d\n", fileno(fp), fileno(outfp));
+
+  if(tcgetattr(fileno(fp), &term) == -1) {
+    perror("tcgetattr");
+    exit(1);
+  }
+  if((term.c_lflag & ECHO) != 0) {
+    echo = 1;
+    term.c_lflag &= ~ECHO;
+    if(tcsetattr(fileno(fp), TCSAFLUSH|TCSASOFT, &term) == -1) {
+      perror("tcsetattr");
+      exit(1);
+    }
+  }
+
+  if(prompt != NULL) {
+    fputs(prompt, outfp);
+  }
+
+  fgets(buf, BUF_SIZE, fp);
+
+  write(fileno(outfp), "\n", 1);
+
+  // restore termios
+  if(echo) {
+    term.c_lflag |= ECHO;
+    tcsetattr(fileno(fp), TCSAFLUSH|TCSASOFT, &term);
+  }
+
+  if(fp != stdin) {
+    fclose(fp);
+  }
+
+  return buf;
+}
+
+int main(void) {
+  char buf[BUF_SIZE];
+  memset(buf, 0, BUF_SIZE);
+  if(getpass_r("Password: ", buf, BUF_SIZE) == NULL) {
+    perror("getpass");
+    exit(1);
+  }
+
+  printf("%s\n", buf);
+  return 0;
+}
